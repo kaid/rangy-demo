3aa399111327dbe41da0a777054c60b04a8d8207
Kaid <kaid@kaid.me>
2014-04-25 15:38:30 +0800

    catch invalid range error

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ window.onload = function() {
     rangy.init()
 
     var serialize   = document.getElementById("serialize")
+      , removeh1    = document.getElementById("removeh1")
       , highlight   = document.getElementById("highlight")
       , buffer      = document.getElementById("buffer").children[0].children[0]
       , hlapplier   = rangy.createCssClassApplier("highlight");
+    
+    removeh1.addEventListener("click", function() {
+        document.getElementsByTagName("h1")[0].remove();
+        removeh1.disabled = true;
+    });
     
     serialize.addEventListener("click", function() {
         if (rangy.getSelection().isCollapsed) return;
@@ -16,8 +22,17 @@ window.onload = function() {
         var serializedSelection = buffer.value;
 
         if (!serializedSelection) return;
-        var selection = rangy.deserializeSelection(serializedSelection).nativeSelection;
-        hlapplier.toggleSelection();
-        selection.collapseToEnd();
+
+        try {
+            rangy.deserializeSelection(serializedSelection);
+            hlapplier.toggleSelection();
+        } catch (e) {
+            var text         = "has no child with index"
+              , rangeinvalid = e.message.indexOf(text) > -1;
+
+            if (rangeinvalid) alert("页面内容已改变，无法恢复选区高亮!");
+        }
+
+        rangy.getSelection().collapseToEnd();
     });
 }
